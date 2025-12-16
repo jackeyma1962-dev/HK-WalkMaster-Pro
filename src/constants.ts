ed6e436ee935fb88453139b848fe8f984122e133
jackey <jackeyma1962@gmail.com>
2025-12-16 20:22:54 +0800

    feat: Switch to Google Maps tile layer
    Updates the map tile layer from CartoDB to Google Maps (roadmap style) for improved map rendering and data.

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,7 @@
 export const DEFAULT_CENTER = { lat: 22.3193, lng: 114.1694 };
 export const DEFAULT_ZOOM = 13;
 
-// Map Attribution
-export const TILE_LAYER_URL = "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
-export const TILE_LAYER_ATTR = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
+// Map Attribution & Tile Layer
+// Switching to Google Maps Tiles (Roadmap style: lyrs=m)
+export const TILE_LAYER_URL = "https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
+export const TILE_LAYER_ATTR = '&copy; <a href="https://www.google.com/maps">Google Maps</a>';
